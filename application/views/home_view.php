a2cbd0370b3779e48e94b3399b96333d6430ad66
unknown <latifnazarudin17@gmail.com>
2021-10-02 12:49:18 +0700

    menambahkan judul pada about

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -120,7 +120,7 @@
 
                 <div class="section-title">
                     <h2>About</h2>
-                    <h3>Find Out More <span>About Us</span></h3>
+                    <h3>PMII <span>Dev.</span></h3>
                     <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
                 </div>
 
