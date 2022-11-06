8525b26ff2ae420ede5de4e5282bccc89981927c
sejutaimpian <derissulistina@gmail.com>
2022-11-06 08:45:50 +0700

    optimize(/): Optimasi db(tbl_about), controller home/index, dan view home_view

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -5,8 +5,7 @@
             <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <h4>Dapatkan informasi terbaru dari kami</h4>
-                    <form action="<?= site_url('subscribe'); ?>" method="post">
-                        <input type="hidden" name="url" value="<?php echo site_url(); ?>" required>
+                    <form action="/subscribe" method="post">
                         <input type="email" name="email" placeholder="Insert your email">
                         <input type="submit" value="Subscribe">
                     </form>
@@ -20,13 +19,11 @@
             <div class="row">
 
                 <div class="col-lg-3 col-md-6 footer-contact">
-                    <h3>Rayon<span>.</span></h3>
+                    <h3><span><?= $site['site_title']; ?></span></h3>
                     <p>
-                        Jl. Taman Amir Hamzah <br>
-                        Pegangsaan, Menteng<br>
-                        Central Jakarta <br><br>
-                        <strong>Phone:</strong> <?= $site_wa; ?><br>
-                        <strong>Email:</strong> <?= $site_mail; ?><br>
+                        <?= $about['about_alamat']; ?><br>
+                        <strong>Phone:</strong> <?= $site['site_wa']; ?><br>
+                        <strong>Email:</strong> <?= $site['site_mail']; ?><br>
                     </p>
                 </div>
 
@@ -52,12 +49,12 @@
 
                 <div class="col-lg-3 col-md-6 footer-links">
                     <h4>Our Social Networks</h4>
-                    <p>Sosial Media <?= $site_title; ?></p>
+                    <p>Sosial Media <?= $site['site_title']; ?></p>
                     <div class="social-links mt-3">
-                        <a href="<?= $site_ig; ?>" class="instagram"><i class="bx bxl-instagram"></i></a>
-                        <a href="<?= $site_fb; ?>" class="facebook"><i class="bx bxl-facebook"></i></a>
-                        <a href="<?= $site_twit; ?>" class="twitter"><i class="bx bxl-twitter"></i></a>
-                        <a href="<?= $site_linked; ?>" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        <a href="<?= $site['site_instagram']; ?>" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="<?= $site['site_facebook']; ?>" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="<?= $site['site_twitter']; ?>" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="<?= $site['site_linkedin']; ?>" class="linkedin"><i class="bx bxl-linkedin"></i></a>
                     </div>
                 </div>
 
@@ -74,3 +71,22 @@
         </div>
     </div>
 </footer>
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="/assets/frontend/vendor/aos/aos.js"></script>
+<script src="/assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/frontend/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/assets/frontend/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/assets/frontend/vendor/php-email-form/validate.js"></script>
+<script src="/assets/frontend/vendor/purecounter/purecounter.js"></script>
+<script src="/assets/frontend/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/assets/frontend/vendor/waypoints/noframework.waypoints.js"></script>
+
+<!-- Template Main JS File -->
+<script src="/assets/frontend/js/main.js"></script>
+
+</body>
+
+</html>
--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -1,42 +1,80 @@
-<section id="topbar" class="d-flex align-items-center">
-    <div class="container d-flex justify-content-center justify-content-md-between">
-        <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:<?= $site_mail; ?>"><?= $site_mail; ?></a></i>
-            <i class="bi bi-phone d-flex align-items-center ms-4"><span><?= $site_wa; ?></span></i>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title><?= $site['site_name']; ?> | <?= $site['site_title']; ?></title>
+    <meta content="<?= $site['site_description']; ?>" name="description">
+    <meta content="PMII, PR, PK, PC, PKC, PB, Pergerakan Mahasiswa Islam Indonesia" name="keywords">
+
+    <!-- Favicons -->
+    <link href="/assets/frontend/img/favicon.png" rel="icon">
+    <link href="/assets/frontend/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="/assets/frontend/vendor/aos/aos.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="/assets/frontend/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: BizLand - v3.5.0
+  * Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    <section id="topbar" class="d-flex align-items-center">
+        <div class="container d-flex justify-content-center justify-content-md-between">
+            <div class="contact-info d-flex align-items-center">
+                <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:<?= $site['site_mail']; ?>"><?= $site['site_mail']; ?></a></i>
+                <i class="bi bi-phone d-flex align-items-center ms-4"><span><?= $site['site_wa']; ?></span></i>
+            </div>
+            <div class="social-links d-none d-md-flex align-items-center">
+                <a href="<?= $site['site_instagram']; ?>" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="<?= $site['site_facebook']; ?>" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="<?= $site['site_twitter']; ?>" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="<?= $site['site_linkedin']; ?>" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+            </div>
         </div>
-        <div class="social-links d-none d-md-flex align-items-center">
-            <a href="<?= $site_ig; ?>" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="<?= $site_fb; ?>" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="<?= $site_twit; ?>" class="twitter"><i class="bi bi-twitter"></i></a>
-            <a href="<?= $site_linked; ?>" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+    </section>
+
+    <header id="header" class="d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <h1 class="logo"><a href="/"><img src="/assets/frontend/images/<?= $site['site_favicon']; ?>"> <span><?= $site['site_name']; ?></span></a></h1>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#about">About</a></li>
+                    <li><a class="nav-link scrollto" href="/gallery">Gallery</a></li>
+                    <li><a class="nav-link scrollto" href="/team">Team</a></li>
+                    <li class="dropdown"><a href="/post"><span>News</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="/category/news">Berita</a></li>
+                            <li><a href="/category/opinion">Opini</a></li>
+                            <li><a href="#">Cerpen</a></li>
+                            <li><a href="#">Puisi</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="/document">Document</a></li>
+                    <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
         </div>
-    </div>
-</section>
-
-<header id="header" class="d-flex align-items-center">
-    <div class="container d-flex align-items-center justify-content-between">
-
-        <h1 class="logo"><a href="/"><img src="<?= '/assets/frontend/images/' . $logo; ?>"> <span><?= $site_name; ?></span></a></h1>
-
-        <nav id="navbar" class="navbar">
-            <ul>
-                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto " href="/gallery">Gallery</a></li>
-                <li><a class="nav-link scrollto" href="/team">Team</a></li>
-                <li class="dropdown"><a href="/post"><span>News</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul>
-                        <li><a href="/category/news">Berita</a></li>
-                        <li><a href="/category/opinion">Opini</a></li>
-                        <li><a href="#">Cerpen</a></li>
-                        <li><a href="#">Puisi</a></li>
-                    </ul>
-                </li>
-                <li><a class="nav-link scrollto" href="/document">Document</a></li>
-                <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav><!-- .navbar -->
-
-    </div>
-</header>
+    </header>
