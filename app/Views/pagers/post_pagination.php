025ea55e74f3ba2df2507d603ac8c182fc1ad49e
irchamali <id.irchamali@gmail.com>
2023-07-21 09:13:24 +0700

    Change the alignment of pagination components

--- app/Views/pagers/post_pagination.php
+++ app/Views/pagers/post_pagination.php
@@ -1,7 +1,7 @@
 <?php $pager->setSurroundCount(2) ?>
 
 <nav aria-label="Page navigation">
-    <ul class="pagination">
+    <ul class="pagination justify-content-center">
     <?php if ($pager->hasPrevious()) : ?>
         <li class="page-item">
             <a class="page-link" href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>">
