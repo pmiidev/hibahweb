475a019eed5c4c7833b53810c38cfffd5bdf84fb
irchamali <id.irchamali@gmail.com>
2025-03-22 18:34:57 +0700

    Add login page and user model

--- app/Config/App.php
+++ app/Config/App.php
@@ -40,7 +40,7 @@ class App extends BaseConfig
      * something else. If you have configured your web server to remove this file
      * from your site URIs, set this variable to an empty string.
      */
-    public string $indexPage = 'index.php';
+    public string $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
--- app/Views/v_login.php
+++ app/Views/v_login.php
@@ -1,85 +1,128 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <!-- Title -->
-    <title>Sign in</title>
-
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta charset="UTF-8">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="Ircham Ali" />
-    <meta name="robots" content="noindex, nofollow">
-    <link rel="shortcut icon" href="/assets/frontend/img/apple-touch-icon.png">
-    <!-- Styles -->
-    <link href="/assets/backend/plugins/pace-master/themes/blue/pace-theme-flash.css" rel="stylesheet" />
-    <link href="/assets/backend/plugins/uniform/css/uniform.default.min.css" rel="stylesheet" />
-    <link href="/assets/backend/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/plugins/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/plugins/line-icons/simple-line-icons.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/plugins/offcanvasmenueffects/css/menu_cornerbox.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/plugins/waves/waves.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/plugins/switchery/switchery.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/plugins/3d-bold-navigation/css/style.css" rel="stylesheet" type="text/css" />
-
-    <!-- Theme Styles -->
-    <link href="/assets/backend/css/modern.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/css/themes/green.css" class="theme-color" rel="stylesheet" type="text/css" />
-    <link href="/assets/backend/css/custom.css" rel="stylesheet" type="text/css" />
-
-    <script src="/assets/backend/plugins/3d-bold-navigation/js/modernizr.js"></script>
-    <script src="/assets/backend/plugins/offcanvasmenueffects/js/snap.svg-min.js"></script>
-
-
-</head>
-
-<body class="page-login">
-    <main class="page-content">
-        <div class="page-inner">
-            <div id="main-wrapper">
-                <div class="row">
-                    <div class="col-md-3 center">
-                        <div class="login-box">
-                            <a href="#" class="logo-name text-lg text-center">PMII LOGIN</a>
-                            <p class="text-center m-t-md">Please login into your account.</p>
-                            <?php if (session()->getFlashData('pesan')) : ?>
-                                <div class="alert alert-warning" role="alert">
-                                    <?= session()->getFlashData('pesan') ?>
-                                </div>
-                            <?php endif; ?>
-                            <form class="m-t-md" action="/login/validasi" method="post">
-                                <div class="form-group">
-                                    <input type="email" class="form-control" name="email" placeholder="Email" required autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control" name="password" placeholder="Password" required>
-                                </div>
-                                <button type="submit" class="btn btn-success btn-block">Login</button>
-
-                            </form>
-                            <p class="text-center m-t-xs text-sm"><?php echo date('Y'); ?> &copy; Design by Ircham Ali.</p>
-                        </div>
-                    </div>
-                </div><!-- Row -->
-            </div><!-- Main Wrapper -->
-        </div><!-- Page Inner -->
-    </main><!-- Page Content -->
-
-
-    <!-- Javascripts -->
-    <script src="/assets/backend/plugins/jquery/jquery-2.1.4.min.js"></script>
-    <script src="/assets/backend/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <script src="/assets/backend/plugins/pace-master/pace.min.js"></script>
-    <script src="/assets/backend/plugins/jquery-blockui/jquery.blockui.js"></script>
-    <script src="/assets/backend/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/assets/backend/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="/assets/backend/plugins/switchery/switchery.min.js"></script>
-    <script src="/assets/backend/plugins/uniform/jquery.uniform.min.js"></script>
-    <script src="/assets/backend/plugins/offcanvasmenueffects/js/classie.js"></script>
-    <script src="/assets/backend/plugins/waves/waves.min.js"></script>
-    <script src="/assets/backend/js/modern.min.js"></script>
-
-</body>
-
+<!doctype html>
+<html lang="en">
+  <!--begin::Head-->
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Login Page</title>
+    <!--begin::Primary Meta Tags-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="title" content="Login" />
+    <meta name="author" content="ColorlibHQ" />
+    <!--end::Primary Meta Tags-->
+    <!--begin::Fonts-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
+      integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
+      crossorigin="anonymous"
+    />
+    <!--end::Fonts-->
+    <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/styles/overlayscrollbars.min.css"
+      integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg="
+      crossorigin="anonymous"
+    />
+    <!--end::Third Party Plugin(OverlayScrollbars)-->
+    <!--begin::Third Party Plugin(Bootstrap Icons)-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+      integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI="
+      crossorigin="anonymous"
+    />
+    <!--end::Third Party Plugin(Bootstrap Icons)-->
+    <!--begin::Required Plugin(AdminLTE)-->
+    <link rel="stylesheet" href="<?= base_url(''); ?>assets/lte4/css/adminlte.css" />
+    <!--end::Required Plugin(AdminLTE)-->
+  </head>
+  <!--end::Head-->
+  <!--begin::Body-->
+  <body class="login-page bg-body-secondary">
+    <div class="login-box">
+      <div class="card card-outline card-primary">
+        <div class="card-header">
+            <?php if (session()->getFlashData('pesan')) : ?>
+            <div class="alert alert-warning" role="alert">
+                <?= session()->getFlashData('pesan') ?>
+            </div>
+            <?php endif; ?>
+        </div>
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+          <form action="/login/validasi" method="post">
+            <div class="input-group mb-1">
+              <div class="form-floating">
+                <input name="email" type="email" class="form-control" value="" placeholder="" />
+                <label for="loginEmail">Email</label>
+              </div>
+              <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+            </div>
+            <div class="input-group mb-1">
+              <div class="form-floating">
+                <input name="password" type="password" class="form-control" placeholder="" />
+                <label for="loginPassword">Password</label>
+              </div>
+              <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+            </div>
+            <div class="social-auth-links text-center mb-3 d-grid gap-2">
+                <br>
+                <button type="submit" class="btn btn-primary"><i class="bi bi-arrow-right-circle-fill"></i> Sign in</button>
+            </div>
+          </form>
+          
+          <!-- /.social-auth-links -->
+          <!-- <p class="mb-1"><a href="#">I forgot my password</a></p>
+          <p class="mb-0"><a href="#" class="text-center"> Register a new account </a></p> -->
+        </div>
+        <!-- /.login-card-body -->
+      </div>
+    </div>
+    <!-- /.login-box -->
+    <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
+      integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
+      crossorigin="anonymous"
+    ></script>
+    <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+      crossorigin="anonymous"
+    ></script>
+    <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+      integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+      crossorigin="anonymous"
+    ></script>
+    <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
+    <script src="<?= base_url(''); ?>assets/lte4/js/adminlte.js"></script>
+    <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+    <script>
+      const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+      const Default = {
+        scrollbarTheme: 'os-theme-light',
+        scrollbarAutoHide: 'leave',
+        scrollbarClickScroll: true,
+      };
+      document.addEventListener('DOMContentLoaded', function () {
+        const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+        if (sidebarWrapper && typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== 'undefined') {
+          OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+            scrollbars: {
+              theme: Default.scrollbarTheme,
+              autoHide: Default.scrollbarAutoHide,
+              clickScroll: Default.scrollbarClickScroll,
+            },
+          });
+        }
+      });
+    </script>
+    <!--end::OverlayScrollbars Configure-->
+    <!--end::Script-->
+  </body>
+  <!--end::Body-->
 </html>
