d2bb355b625312c95c3656a6a212491737cd5e05
irchamali <id.irchamali@gmail.com>
2026-04-11 10:48:41 +0700

    Update layouts and home

---
+++ app/Views/layouts/home-header.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Index - PMII</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Favicons -->
+    <link href="assets/blogy/img/favicon.png" rel="icon">
+    <link href="assets/blogy/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/blogy/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/blogy/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/blogy/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/blogy/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/blogy/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/blogy/css/main.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: Blogy
+  * Template URL: https://bootstrapmade.com/blogy-bootstrap-blog-template/
+  * Updated: Feb 22 2025 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="index-page">
+
+    <header id="header" class="header position-relative">
+        <div class="container-fluid container-xl position-relative">
+
+            <div class="top-row d-flex align-items-center justify-content-between">
+                <a href="index.html" class="logo d-flex align-items-end">
+                    <!-- Uncomment the line below if you also wish to use an image logo -->
+                    <!-- <img src="assets/blogy/img/logo.webp" alt=""> -->
+                    <h1 class="sitename">DEV</h1><span>.</span>
+                </a>
+
+                <div class="d-flex align-items-center">
+                    <div class="social-links">
+                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                    </div>
+
+                    <form class="search-form ms-4">
+                        <input type="text" placeholder="Search..." class="form-control">
+                        <button type="submit" class="btn"><i class="bi bi-search"></i></button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="nav-wrap">
+            <div class="container d-flex justify-content-center position-relative">
+                <nav id="navmenu" class="navmenu">
+                    <ul>
+                        <li><a href="index.html" class="active">Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="category.html">Category</a></li>
+                        <li><a href="blog-details.html">Blog Details</a></li>
+                        <li><a href="author-profile.html">Author Profile</a></li>
+                        <li class="dropdown"><a href="#"><span>Pages</span> <i
+                                    class="bi bi-chevron-down toggle-dropdown"></i></a>
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="category.html">Category</a></li>
+                                <li><a href="blog-details.html">Blog Details</a></li>
+                                <li><a href="author-profile.html">Author Profile</a></li>
+                                <li><a href="search-results.html">Search Results</a></li>
+                                <li><a href="404.html">404 Not Found Page</a></li>
+
+                                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i
+                                            class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                    <ul>
+                                        <li><a href="#">Deep Dropdown 1</a></li>
+                                        <li><a href="#">Deep Dropdown 2</a></li>
+                                        <li><a href="#">Deep Dropdown 3</a></li>
+                                        <li><a href="#">Deep Dropdown 4</a></li>
+                                        <li><a href="#">Deep Dropdown 5</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                    <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                </nav>
+            </div>
+        </div>
+
+    </header>
